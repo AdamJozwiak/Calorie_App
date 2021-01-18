2c267b3c7ca8d16e18fc856657f35ed7415d8c33
Adam <adamj500@interia.pl>
2021-01-18 18:59:43 +0100

    Modify API to return food object

--- lib/services/calorie_api.dart
+++ lib/services/calorie_api.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-
+import 'package:call_app/models/food.dart';
+import 'package:call_app/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Api extends StatefulWidget {
   @override
@@ -14,19 +14,18 @@ class _ApiState extends State<Api> {
   String APP_KEY = 'a3f88b680a73f38bc741e86157d7c378';
   String foodName = '';
 
-  void getFoodInfo() async
-  {
-    Response response =
-    await get('https://api.edamam.com/api/food-database/v2/parser?ingr=${foodName}&app_id=${APP_ID}&app_key=${APP_KEY}');
+  void getFoodInfo() async {
+    Response response = await get(
+        'https://api.edamam.com/api/food-database/v2/parser?ingr=${foodName}&app_id=${APP_ID}&app_key=${APP_KEY}');
     Map data = jsonDecode(response.body);
 
-    Map extractedData = {
-      'image': data['parsed'][0]['food']['image'],
-      'name': data['text'],
-      'kcal': data['parsed'][0]['food']['nutrients']['ENERC_KCAL'],
-      'fat': data['parsed'][0]['food']['nutrients']['FAT'],
-      'amount': 1,
-    };
+    Food extractedData = Food(
+      name: data['text'],
+      imageUrl: data['parsed'][0]['food']['image'],
+      kcal: data['parsed'][0]['food']['nutrients']['ENERC_KCAL'],
+      fat: data['parsed'][0]['food']['nutrients']['FAT'],
+      amount: 1,
+    );
 
     Navigator.pop(context, extractedData);
   }
@@ -35,14 +34,6 @@ class _ApiState extends State<Api> {
   Widget build(BuildContext context) {
     this.foodName = ModalRoute.of(context).settings.arguments;
     getFoodInfo();
-    return Scaffold(
-      backgroundColor: Colors.grey[350],
-      body: Center(
-        child: SpinKitDualRing(
-          color: Colors.green[600],
-          size: 120.0,
-        ),
-      ),
-    );
+    return Loading();
   }
 }
