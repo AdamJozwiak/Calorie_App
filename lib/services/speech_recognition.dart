1f9093364e42b2732d4d7060f68af9bfa6786904
Adam <adamj500@interia.pl>
2021-01-18 18:46:10 +0100

    Modify speech rec pattern

--- lib/services/speech_recognition.dart
+++ lib/services/speech_recognition.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:call_app/shared/showAlert.dart';
 
 class SpeechRec extends StatefulWidget {
+  final getFoodname;
+  SpeechRec({Key key, this.getFoodname}) : super(key: key);
+
   @override
   _SpeechRecState createState() => _SpeechRecState();
 }
@@ -10,7 +14,6 @@ class SpeechRec extends StatefulWidget {
 class _SpeechRecState extends State<SpeechRec> {
   stt.SpeechToText _speech;
   bool _isListening;
-  double _confidence;
   String _text;
 
   @override
@@ -18,64 +21,84 @@ class _SpeechRecState extends State<SpeechRec> {
     super.initState();
     _speech = stt.SpeechToText();
     _isListening = false;
-    _confidence = 1.0;
     _text = '';
   }
 
   void listen() async {
-    if(!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val'),
-        onError: (val) => print('onError: $val'),
-      );
-      if(available) {
-        setState(() {
-          _isListening = true;
-        });
-        _speech.listen(
-          onResult: (val) => setState(() {
-            _text = val.recognizedWords;
-            if(val.hasConfidenceRating && val.confidence > 0) {
-              _confidence = val.confidence;
-            }
-          }),
-        );
-      }
-    } else {
+    bool available = await _speech.initialize(
+      onStatus: (val) => print('onStatus: $val'),
+      onError: (val) => print('onError: $val'),
+    );
+    if (available) {
       setState(() {
-        _isListening = false;
+        _isListening = true;
       });
-      _speech.stop();
+      _speech.listen(onResult: (val) {
+        setState(() {
+          _text = val.recognizedWords;
+          if (_text != null && _text != '') {
+            _text = _text.substring(0, _text.indexOf(' '));
+          }
+        });
+      });
     }
+  }
+
+  void stopListening() async {
+    setState(() {
+      _isListening = false;
+    });
+    _speech.stop();
+    widget.getFoodname(_text);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
-      child: Center(
-        child: Column(
-          children: [
-            Text(_text),
-            Container(
-              child: AvatarGlow(
-                animate: _isListening,
-                glowColor: Colors.green,
-                endRadius: 75.0,
-                duration: const Duration(milliseconds: 2000),
-                repeatPauseDuration: const Duration(milliseconds: 100),
-                repeat: true,
-                child: FloatingActionButton(
-                  onPressed: () {
+    return Center(
+      child: Column(
+        children: [
+          Text(_text),
+          Container(
+            child: AvatarGlow(
+              animate: _isListening,
+              glowColor: Colors.green,
+              endRadius: 75.0,
+              duration: const Duration(milliseconds: 2000),
+              repeatPauseDuration: const Duration(milliseconds: 100),
+              repeat: true,
+              child: Listener(
+                onPointerDown: (_) {
+                  if (!_isListening) {
                     listen();
-                    },
-                child: Icon(Icons.mic),
-                backgroundColor: Colors.green[700],
+                  } else {
+                    showAlert(
+                        context, 'Error', 'An error occured. Please try again');
+                  }
+                },
+                onPointerUp: (_) {
+                  if (_isListening) {
+                    stopListening();
+                  } else {
+                    showAlert(context, 'Cannot find result',
+                        'Please, try holding the button a little longer');
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green[700],
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  padding: EdgeInsets.all(15.0),
+                  child: Icon(
+                    Icons.mic,
+                    size: 30,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
