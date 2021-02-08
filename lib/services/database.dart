d67938eadf6d973f5ca451ea77e8a7ade0db9617
Adam <adamj500@interia.pl>
2021-02-08 21:23:18 +0100

    Add function returning food from specific date

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,10 +1,10 @@
 import 'package:call_app/models/food.dart';
-import 'package:call_app/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:call_app/shared/functions.dart';
 
 class DatabaseService {
   final String uid;
+  String selectedDate = getCurrentDate();
   DatabaseService({this.uid});
 
   // collection reference
@@ -12,37 +12,61 @@ class DatabaseService {
       Firestore.instance.collection('food');
 
   Future updateUserData(Food food) async {
-    return await foodCollection
-        .document(uid)
-        .collection(getCurrentDate())
-        .document(food.name)
-        .setData({
-      'name': food.name ?? '',
-      'kcal': food.kcal ?? '0',
-      'fat': food.fat ?? '0',
-      'image': food.imageUrl ?? null,
-      'amount': food.amount ?? 0
-    });
-  }
-
-  List<Food> _foodListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.documents.map((doc) {
-      return Food(
-        name: doc.data['name'] ?? '',
-        kcal: doc.data['kcal'] ?? '0',
-        fat: doc.data['fat'] ?? '0',
-        imageUrl: doc.data['image'] ?? null,
-        amount: doc.data['amount'] ?? 0,
-      );
-    }).toList();
+    if (food != null) {
+      return await foodCollection
+          .document(uid)
+          .collection(selectedDate)
+          .document(food.name)
+          .setData({
+        'name': food.name ?? '',
+        'kcal': food.kcal ?? '0',
+        'fat': food.fat ?? '0',
+        'image': food.imageUrl ?? null,
+        'amount': food.amount ?? 0
+      });
+    } else {
+      throw Exception('No food data transfered to database');
+    }
   }
 
   // get food stream
   Stream<List<Food>> get foods {
     return foodCollection
         .document(uid)
-        .collection(getCurrentDate())
+        .collection(selectedDate)
         .snapshots()
         .map(_foodListFromSnapshot);
+  }
+
+  List<Food> _foodListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Food(
+        name: doc.data['name'] ?? '',
+        kcal: doc.data['kcal'] ?? 0,
+        fat: doc.data['fat'] ?? 0,
+        imageUrl: doc.data['image'] ?? null,
+        amount: doc.data['amount'] ?? 0,
+      );
+    }).toList();
+  }
+
+  Future<List<Food>> getUserData(String date) async {
+    List<Food> foods = new List();
+    return await foodCollection
+        .document(uid)
+        .collection(date)
+        .getDocuments()
+        .then((value) {
+      value.documents.forEach((doc) {
+        foods.add(Food(
+          name: doc.data['name'] ?? '',
+          kcal: doc.data['kcal'] ?? 0,
+          fat: doc.data['fat'] ?? 0,
+          imageUrl: doc.data['image'] ?? null,
+          amount: doc.data['amount'] ?? 0,
+        ));
+      });
+      return foods;
+    });
   }
 }
